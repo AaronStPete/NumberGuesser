6e6c2cbf94651fab48f9519affd50735050c1754
AaronStPete <AaronStPete@users.noreply.github.com>
2018-04-03 21:40:14 -0400

    Game is built and plays through once

--- NumberGuesser/Program.cs
+++ NumberGuesser/Program.cs
@@ -9,7 +9,7 @@ namespace NumberGuesser
             ///intro
             Console.WriteLine("Hey Player! Bet you can't guess my number!");
 
-            ///TODO: asking for a random number range
+            ///asking for a random number range
             Console.WriteLine("How hard do you want to make it? Pick a range:");
             Console.WriteLine("What's the lowest end of the range?");
             int floorRange = Convert.ToInt32(Console.ReadLine());
@@ -26,13 +26,25 @@ namespace NumberGuesser
             ///run random number program
             Random rnd = new Random();
             int computerNumber = rnd.Next(floorRange, ceilRange);
-            Console.WriteLine($"I've chosen {computerNumber}. It's a secret...");
+            ///Console.WriteLine($"I've chosen {computerNumber}. It's a secret...");
 
             Console.WriteLine("What are you sitting around for?");
             ///input field for player to input their guess
             Console.WriteLine($"Pick a number between {floorRange} and {ceilRange}:");
             int playerGuess = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine($"So you think my number is {playerGuess}?");
+
+            ///game logic
+            if (playerGuess == computerNumber)
+            {
+                Console.WriteLine("You got it right? Wow!");
+            }   else if (playerGuess < computerNumber)
+            {
+                Console.WriteLine("You guessed too low. Bummer...");
+            }   else if (playerGuess > computerNumber)
+            {
+                Console.WriteLine("You overshot the mark! Overachiever...");
+            }
 
             Console.ReadLine();
         }
