2f0856c5aa1269b195722321254839f569073312
AaronStPete <AaronStPete@users.noreply.github.com>
2018-04-04 11:11:44 -0400

    game automatically replays

--- NumberGuesser/Program.cs
+++ NumberGuesser/Program.cs
@@ -9,44 +9,48 @@ namespace NumberGuesser
             ///intro
             Console.WriteLine("Hey Player! Bet you can't guess my number!");
 
-            ///asking for a random number range
-            Console.WriteLine("How hard do you want to make it? Pick a range:");
-            Console.WriteLine("What's the lowest end of the range?");
-            int floorRange = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("What's the highest end of the range?");
-            int ceilRange = Convert.ToInt32(Console.ReadLine());
-
-            ///display range:
-            Console.WriteLine($"So you wanna play between {floorRange} and {ceilRange} huh?");
-
-            ///generating a random number between 1+10
-            Console.WriteLine("Fine, I'm picking a number. Hold tight...");
-
-            ///run random number program
-            Random rnd = new Random();
-            int computerNumber = rnd.Next(floorRange, ceilRange);
-            ///Console.WriteLine($"I've chosen {computerNumber}. It's a secret...");
-
-            Console.WriteLine("What are you sitting around for?");
-            ///input field for player to input their guess
-            Console.WriteLine($"Pick a number between {floorRange} and {ceilRange}:");
-            int playerGuess = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine($"So you think my number is {playerGuess}?");
-
-            ///game logic
-            if (playerGuess == computerNumber)
+            while (true)
             {
-                Console.WriteLine("You got it right? Wow!");
-            }   else if (playerGuess < computerNumber)
-            {
-                Console.WriteLine("You guessed too low. Bummer...");
-            }   else if (playerGuess > computerNumber)
-            {
-                Console.WriteLine("You overshot the mark! Overachiever...");
+                ///asking for a random number range
+                Console.WriteLine("How hard do you want to make it? Pick a range:");
+                Console.WriteLine("What's the lowest end of the range?");
+                int floorRange = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("What's the highest end of the range?");
+                int ceilRange = Convert.ToInt32(Console.ReadLine());
+
+                ///display range:
+                Console.WriteLine($"So you wanna play between {floorRange} and {ceilRange} huh?");
+
+                ///generating a random number between 1+10
+                Console.WriteLine("Fine, I'm picking a number. Hold tight...");
+
+                ///run random number program
+                Random rnd = new Random();
+                int computerNumber = rnd.Next(floorRange, ceilRange);
+                ///Console.WriteLine($"I've chosen {computerNumber}. It's a secret...");
+
+                Console.WriteLine("What are you sitting around for?");
+                ///input field for player to input their guess
+                Console.WriteLine($"Pick a number between {floorRange} and {ceilRange}:");
+                int playerGuess = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine($"So you think my number is {playerGuess}?");
+
+                ///game logic
+                if (playerGuess == computerNumber)
+                {
+                    Console.WriteLine("You got it right? Wow!");
+                }
+                else if (playerGuess < computerNumber)
+                {
+                    Console.WriteLine("You guessed too low. Bummer...");
+                }
+                else if (playerGuess > computerNumber)
+                {
+                    Console.WriteLine("You overshot the mark! Overachiever...");
+                }
+                Console.WriteLine("Care to try again?");
             }
-
-            Console.ReadLine();
         }
     }
 }
